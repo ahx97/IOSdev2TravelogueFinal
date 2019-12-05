34a986d13b5f77aefc031650dcc9eb224c409e95
Aaron Henry <ahx97@mail.missouri.edu>
2019-12-05 13:22:34 -0600

    little set up work

---
+++ travelogue/NewTripViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewTripViewController.swift
+//  travelogue
+//
+//  Created by Aaron Henry on 12/5/19.
+//  Copyright © 2019 Aaron Henry. All rights reserved.
+//
+
+import UIKit
+
+class NewTripViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
